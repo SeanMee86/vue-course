715f5e22a7c49b0164deff2f460ca85f734ba4df
SeanMee86 <seanmee8186@gmail.com>
2020-11-08 10:36:59 -0800

    updating course

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -1,15 +1,42 @@
+function twoSecondReset() {
+  this.counter = 0;
+}
+
 const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+      // fullname: ''
     };
+  },
+  watch: {
+    counter(v) {
+      if(v > 50) {
+        setTimeout(function() {this.counter = 0; console.log(this)}.bind(this), 2000);
+      }
+    }
+    // name(v) {
+    //   if(v === '' ){
+    //     this.fullname = '';
+    //   }else{
+    //     this.fullname = `${v} ${this.lastName}`
+    //   }
+    // },
+    // lastName(v) {
+    //   if(v === '' ){
+    //     this.fullname = '';
+    //   }else{
+    //     this.fullname = `${this.name} ${v}`
+    //   }
+    // }
   },
   computed: {
     fullname() {
-      if (this.name === '')
+      if (this.name === '' || this.lastName === '')
         return '';
-      return this.name+' '+'Mee';
+      return `${this.name} ${this.lastName}`;
     }
   },
   methods: {
